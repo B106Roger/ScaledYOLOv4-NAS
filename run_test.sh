18f30d3a121f7e8e29076ce75c5695cba81f6eaa
Sawersvy <Sawersvy1997@gmail.com>
2022-10-25 13:29:38 +0800

    add Egor's Config

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
 echo test_start
-python ./test.py --img-size 416 --conf 0.001 --batch 16 --device 1 --data ./data/voc.yaml --weights ./runs_1016_csp/exp0/weights/best.pt
+python ./test.py --img-size 416 --conf 0.001 --batch 16 --device 1 --data ./data/voc.yaml --weights ./runs_1024_csp_0.75/exp0/weights/best.pt
